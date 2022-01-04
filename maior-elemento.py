01f4d30bf5d78751b8f7cfdd77626c9e79c63ec9
MFlister <michael.flister.mf@gmail.com>
2022-01-04 18:51:09 -0300

    Excluindo arquivos que foram adicionados acidentalmente

--- maior-elemento.py
+++
@@ -1,8 +0,0 @@
-# Desenvolva uma função recursiva que calcule e mostre o maior elemento de um array de tamanho N. Os elementos consistem em números inteiros.
-def max_list(lista:list):
-    if len(lista)==1:
-        return lista[0]
-    else:
-        maximo = max_list(lista[1:])
-        #print(maximo)
-        return maximo if maximo > lista[0] else lista[0]
