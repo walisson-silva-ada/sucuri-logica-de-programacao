c644c14d89062b20a24da7cc40b31ebae9366316
MFlister <michael.flister.mf@gmail.com>
2022-01-04 18:39:07 -0300

    Adicionado função super digito

---
+++ super_digito.py
@@ -0,0 +1,16 @@
+def super_digito(x:int) ->int:
+    x=str(x)
+    if len(x)==1:
+        return int(x)
+    else:
+        soma = 0
+        for digito in list(x):
+            soma+= int(digito)
+        if len(str(soma)) == 1:
+            return soma
+        else:
+             return super_digito(soma)
+
+def main(n:int, k:int) -> int :
+    numero = int(str(n)*k)
+    return super_digito(numero)
